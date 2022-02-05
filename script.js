1322155cdf8230fc7e03266520967a928f6efa8e
dulakshanwije <dulakshanwije@gmail.com>
2022-02-05 18:39:14 +0530

    Simple UI added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
+var count = 0;
 function jump(){
     character.classList.add("jump");
     setTimeout(function(){
@@ -8,27 +9,41 @@ function jump(){
 }
 
 setInterval(function(){
-    // var blocker_top = parseInt(window.getComputedStyle(block).getPropertyValue("right"));
     var blocker_left = block.offsetLeft;
     var blocker_top = block.offsetTop;
     var character_left = character.offsetLeft;
     var character_top = character.offsetTop;
-    // var character_top = parseInt(window.getComputedStyle(character).getPropertyValue("right"));
-    // console.log(blocker_top);
-    // console.log(character_top);
-    
-    // if((character_left - blocker_left) > 5 && character_top >= 390){
-    //     console.log("Done");
-    // }
-    if(Math.abs(character_left - blocker_left) < 30){
-        if(Math.abs(character_top - blocker_top) > 80){
+
+    var count_dis = document.getElementById("count");
+    var pro_bar = document.getElementById("progress_bar");
+
+    var gameover = document.getElementById("gameover");
+    var gamelost = document.getElementById("gamelost");
+
+
+    if(Math.abs(character_left - blocker_left) < 50){
+        if(Math.abs(character_top - blocker_top) > 100){
             console.log(Math.abs(character_top - blocker_top));
+            count++;
+            count_dis.innerHTML = count;
+            pro_bar.value = count;
+            if(count >= 300){
+                block.style.display = "none";
+           character.style.display = "none";
+            gameover.style.display = "flex";
+            count = 0;
+            count_dis.innerHTML = count;
+            pro_bar.value = count;
+            }
         }
         else{
            block.style.display = "none";
-            alert("Failed");
+           character.style.display = "none";
+            gamelost.style.display = "flex";
+            count = 0;
+            count_dis.innerHTML = count;
+            pro_bar.value = count;
         }
     }
-    // console.log(blocker_left);
 },10);
 
